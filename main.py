de278393c5fbf26e5a4948fd0f7471dc792665cc
Miami-stack <muratajmedov@gmail.com>
2020-12-05 10:35:07 +0500

    add change

--- main.py
+++ main.py
@@ -77,6 +77,7 @@ while True:
     if rand == '3':
         print("Вы нашли меч, Введите: 1 что бы взять меч себе выбросив старый, 2 что бы пройти мимо меча")
         nm = exception()
+        print(nm)
         if nm == 1:
             knight_attack = plus(knight_attack, new_attack_knight)
             print(f" Вы взяли меч -> Новая атака рыцаря:   {knight_attack}")
